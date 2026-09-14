f0e13117d25c3635674ab2ee16263f2453294944
Pija14 <rafaellouzadaa@gmail.com>
2026-09-14 17:49:37 -0300

    Add config.example.js

---
+++ config.example.js
@@ -0,0 +1,6 @@
+// Copy this file to config.js and set the public URL of your deployed backend.
+// Do not put API keys in this file.
+window.MEU_TREINO_CONFIG = {
+  AI_API_URL: "https://seu-backend.exemplo.com"
+};
+
