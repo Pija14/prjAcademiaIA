4fa33ef6d365f04e1430f33d5a4678d8565161dc
Pija14 <rafaellouzadaa@gmail.com>
2026-09-15 23:22:38 -0300

    chore: bump PWA cache for GymIA login

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "meu-treino-v52";
+const CACHE = "meu-treino-v53";
 const ASSETS = ["./", "./index.html", "./styles.css", "./home-layout.css", "./app.js", "./manifest.json", "./icons/icon.svg", "./assets/gymia-logo.svg"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
