ca0a95c91f82f32a714a240c60b78728b05d564e
Pija14 <rafaellouzadaa@gmail.com>
2026-09-16 00:29:38 -0300

    Forca atualizacao do cache do PWA para v64

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "meu-treino-v63";
+const CACHE = "meu-treino-v64";
 const ASSETS = ["./", "./index.html", "./styles.css?v=60", "./home-layout.css?v=60", "./login.css?v=63", "./app.js?v=60", "./manifest.json?v=63", "./assets/logo-gymia.svg?v=63"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
