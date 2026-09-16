ee0f65f2ce3979cbe213e56d17d9f6a77c5bccff
Pija14 <rafaellouzadaa@gmail.com>
2026-09-16 00:04:00 -0300

    chore: update PWA cache for login logo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "meu-treino-v59";
-const ASSETS = ["./", "./index.html", "./styles.css", "./home-layout.css", "./login.css", "./app.js", "./manifest.json", "./icons/icon.svg"];
+const CACHE = "meu-treino-v60";
+const ASSETS = ["./", "./index.html", "./styles.css", "./home-layout.css", "./login.css", "./app.js", "./manifest.json", "./icons/icon.svg", "./assets/logo-gymia.svg"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener("fetch", e => {
