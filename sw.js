47142b1a2bd672a448f83b2df8a368564ef5c542
Pija14 <rafaellouzadaa@gmail.com>
2026-09-16 00:16:53 -0300

    Atualiza cache PWA após troca do logo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "meu-treino-v61";
-const ASSETS = ["./", "./index.html", "./styles.css", "./home-layout.css", "./login.css", "./app.js", "./manifest.json", "./icons/icon.svg", "./assets/logo-gymia.svg"];
+const CACHE = "meu-treino-v62";
+const ASSETS = ["./", "./index.html", "./styles.css", "./home-layout.css", "./login.css", "./app.js", "./manifest.json", "./assets/logo-gymia.svg"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener("fetch", e => {
