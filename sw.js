af4b23f804088fc941f82124860756c7774e38a0
Pija14 <rafaellouzadaa@gmail.com>
2026-09-22 20:56:39 -0300

    fix: invalidar cache antigo do PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "meu-treino-v50";
+const CACHE = "meu-treino-v54";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icons/icon.svg"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
@@ -7,4 +7,3 @@ self.addEventListener("fetch", e => {
   if (new URL(e.request.url).origin !== self.location.origin) return;
   e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
 });
-
